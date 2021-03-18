e95bac47f79e89e6409389c49d93ae0895e772c3
sakshigupta265 <itzsakshi26gupta@gmail.com>
2021-03-18 21:57:27 +0530

    Added shop UI
    - added various labels like: residences, book stores, sports shop, library, schools, etc.
    - added interior shop UI for flower shop, candy shop, sports shop
    - bug fixes

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,8 +5,7 @@ public class CameraController : MonoBehaviour {
 
     public GameObject player;        //Public variable to store a reference to the player game object
 
-
-    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+    private Vector3 offset;          //Private variable to store the offset distance between the player and camera
 
     // Use this for initialization
     void Start () 
